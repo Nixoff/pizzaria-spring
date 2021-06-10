aa9ef7744ff869c0fd0e3565a9ff56ed86df086a
nixoff <gabadev48@protonmail.com>
2021-06-10 18:11:18 -0300

    :tada: metodo post funcionando

--- src/main/java/com/nixoff/pizzaria/resources/PizzaResource.java
+++ src/main/java/com/nixoff/pizzaria/resources/PizzaResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +27,12 @@ public class PizzaResource {
 	
 	
 	@GetMapping("/pizza/{id}")
-	public Pizza listaPizzaUnica(@PathVariable(value="id") long id){
+	public Pizza listaPizzaUnica(@PathVariable(value="id") long id) {
 		return pizzaRepository.findById(id);
+	}
+	
+	@PostMapping("pizza")
+	public Pizza salvaPizza(@RequestBody Pizza pizza) {
+		return pizzaRepository.save(pizza);
 	}
 }
